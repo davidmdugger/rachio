7486ea603a48bfdc517bd6e8444632fb4a80e606
davidmd <davidmdugger@gmail.com>
2019-03-24 09:54:36 -0500

    configured webpack, babel, and initial setup for React and ES6+

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HWP = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "/src/index.js"),
+  output: {
+    filename: "build.js",
+    path: path.join(__dirname, "/dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 8000
+  },
+  plugins: [new HWP({ template: path.join(__dirname, "/src/index.html") })]
+};
